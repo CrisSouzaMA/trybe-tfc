2ce8807bc75609c973df62e31e9caaa0e97e5ab3
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2022-05-31 22:54:20 -0300

    Feat: cria jwt com criação de token e decodificação

--- app/backend/src/jwt.ts
+++ app/backend/src/jwt.ts
@@ -2,9 +2,8 @@ import jwt = require('jsonwebtoken');
 import fs = require('fs');
 
 const senhasecreta = fs.readFileSync('./jwt.evaluation.key', { encoding: 'utf-8' });
-console.log(senhasecreta);
 
-export const createToken = (user: object) => {
+export const createToken = (user: object | null) => {
   const token = jwt.sign({ data: user }, senhasecreta, {
     expiresIn: '20d',
     algorithm: 'HS256',

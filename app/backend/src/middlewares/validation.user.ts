fb42a9a0b305e8c5c47269f6b5c1760e52eea23c
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2022-06-01 18:57:36 -0300

    Feat: implementa checagem de erro para passar http status correto

--- app/backend/src/middlewares/validation.user.ts
+++ app/backend/src/middlewares/validation.user.ts
@@ -6,6 +6,9 @@ export default class ValidationUser {
   public validation = (req: Request, res: Response, next: NextFunction, schema: Schema) => {
     const { error } = schema.validate(req.body);
     if (error) {
+      if (error.details[0].type === 'string.email') {
+        return res.status(StatusCodes.UNAUTHORIZED).json({ message: error.details[0].message });
+      }
       return res.status(StatusCodes.BAD_REQUEST).json({ message: error.details[0].message });
     }
     next();

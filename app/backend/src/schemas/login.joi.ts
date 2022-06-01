11be32f4028cf6f0e230693c3bed52da5c3cc6be
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2022-06-01 18:57:59 -0300

    Fix: retorna com tipo email

--- app/backend/src/schemas/login.joi.ts
+++ app/backend/src/schemas/login.joi.ts
@@ -1,8 +1,10 @@
 import Joi = require('joi');
 
 const validateLogin = Joi.object().keys({
-  email: Joi.string().empty().required()
-    .messages({ 'string.empty': 'All fields must be filled' }),
+  email: Joi.string().email().empty().required()
+    .messages({
+      'string.empty': 'All fields must be filled',
+      'string.email': 'Incorrect email or password' }),
   password: Joi.string().min(6).required()
     .messages({ 'string.empty': 'All fields must be filled' }),
 });

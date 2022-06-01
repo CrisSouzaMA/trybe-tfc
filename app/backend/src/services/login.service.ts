2911056f0304fd7e75ca6b888295f209129b8d8d
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2022-05-31 22:53:11 -0300

    Feat: cria service de login

--- app/backend/src/services/login.service.ts
+++ app/backend/src/services/login.service.ts
@@ -1,1 +1,14 @@
-import userModel from '../database/models/user';
+import IUser from '../interfaces/IUser';
+import User from '../database/models/user';
+
+export default class Login {
+  public _result: IUser | null;
+
+  public async login(email: string, password: string): Promise<IUser | null> {
+    this._result = await User
+      .findOne({ where: { email, password }, attributes: { exclude: ['password'] } });
+
+    if (!this._result) throw new Error('Incorrect email or password');
+    return this._result;
+  }
+}

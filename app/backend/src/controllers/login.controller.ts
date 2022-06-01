45249794aacd5aa1e0b25ae02e13c9405db86dff
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2022-05-31 22:49:38 -0300

    Feat: cria pasta de controllers

---
+++ app/backend/src/controllers/login.controller.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import Login from '../services/login.service';
+import { createToken } from '../jwt';
+
+export default class LoginController {
+  private service = new Login();
+
+  public login = async (req: Request, res: Response) => {
+    try {
+      const { email, password } = req.body;
+      const user = await this.service.login(email, password);
+      const token = createToken(user);
+      return res.status(StatusCodes.OK).json({ user, token });
+    } catch (error) {
+      const e = error instanceof Error && error.message;
+      return res.status(StatusCodes.BAD_REQUEST).json({ message: e });
+    }
+  };
+}

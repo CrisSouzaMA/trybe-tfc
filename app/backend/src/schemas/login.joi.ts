70a0b184f205c59c159df4b5f707bea0d281a126
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2022-05-31 22:53:41 -0300

    Feat: cria joi de validação do login

--- app/backend/src/schemas/login.joi.ts
+++ app/backend/src/schemas/login.joi.ts
@@ -1,8 +1,8 @@
 import Joi = require('joi');
 
 const validateLogin = Joi.object({
-  username: Joi.string().required(),
-  password: Joi.string().required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
 });
 
 export default validateLogin;

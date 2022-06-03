3919c9131647a42e1e937484dfecd28bf9f2eb3e
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2022-06-03 19:34:46 -0300

    Feat: cria mock de usuario para iniciar os testes

---
+++ app/backend/src/tests/mocks/user.ts
@@ -0,0 +1,13 @@
+import IUser from '../../interfaces/IUser';
+
+const user: IUser = {
+  id: 1,
+  username: 'HelloKitty',
+  role: 'admin',
+  email: 'hellokitty@hellokitty.com',
+  password: '$2a$12$KkpLXfcxyu2.dRKvOMJ2J..ioxORI49LJ7.9oc2ruLttOtms2zmXi', //secret_admin
+}
+
+const Usermockado = user;
+
+export default Usermockado;

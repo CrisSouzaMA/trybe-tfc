48e4f937240d7c2ba67bbdf5c4aa575594258233
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2022-06-04 15:13:43 -0300

    Feat: implementa service da rota /teams/:id

--- app/backend/src/services/team.service.ts
+++ app/backend/src/services/team.service.ts
@@ -3,9 +3,15 @@ import ITeam from '../interfaces/ITeam';
 
 export default class Teams {
   private _result: ITeam[] | null;
+  private _resultId: ITeam | null;
 
   public async allteam() {
     this._result = await Team.findAll();
     return this._result;
+  }
+
+  public async teambyid(id: string) {
+    this._resultId = await Team.findByPk(id);
+    return this._resultId;
   }
 }

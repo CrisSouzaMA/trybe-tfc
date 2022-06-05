c2e0d9df15ed850e90744dcd670359c46d831402
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2022-06-05 12:52:46 -0300

    Feat: implementando requisitos 20 e 21 com condicional de query para renderização das telas na partida de acordo com o progresso

--- app/backend/src/controllers/match.controller.ts
+++ app/backend/src/controllers/match.controller.ts
@@ -5,8 +5,21 @@ import Matchs from '../services/match.service';
 export default class MatchController {
   private _service = new Matchs();
 
-  public matchs = async (_req: Request, res: Response) => {
+  public matchs = async (req: Request, res: Response) => {
+    const { inProgress } = req.query;
+
     const allmatches = await this._service.matches();
+
+    const matchtrue = allmatches.filter((m) => m.inProgress === true);
+    const matchfalse = allmatches.filter((m) => m.inProgress === false);
+
+    if (inProgress && inProgress === 'true') {
+      return res.status(StatusCodes.OK).json(matchtrue);
+    }
+    if (inProgress && inProgress === 'false') {
+      return res.status(StatusCodes.OK).json(matchfalse);
+    }
+
     return res.status(StatusCodes.OK).json(allmatches);
   };
 }

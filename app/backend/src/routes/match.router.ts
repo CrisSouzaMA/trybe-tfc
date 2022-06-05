63d60cd9a731487b11e7c923f2ab97ff8ceae889
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2022-06-05 14:12:06 -0300

    Feat: implementa rota de match

--- app/backend/src/routes/match.router.ts
+++ app/backend/src/routes/match.router.ts
@@ -1,13 +1,21 @@
 import { Application } from 'express';
 import MatchController from '../controllers/match.controller';
+import ValidationToken from '../middlewares/validation.token';
 
 export default class MatchRouter {
   private controller = new MatchController();
+  public validationToken = new ValidationToken();
 
   public route = (app: Application): void => {
     app.get(
       '/matches',
       (req, res) => this.controller.matchs(req, res),
+    );
+
+    app.post(
+      '/matches',
+      (req, res, next) => this.validationToken.validationtoken(req, res, next),
+      (req, res) => this.controller.creatematch(req, res),
     );
   };
 }

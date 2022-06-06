089e5daae6c08fb02869443b4b4ae362c5807d27
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2022-06-06 15:37:31 -0300

    Feat: implementa teste da rota /matches/:id/finish

--- app/backend/src/tests/match.test.ts
+++ app/backend/src/tests/match.test.ts
@@ -11,6 +11,7 @@ import AllMatchesMock from './mocks/matches';
 import Creatematchmock from './mocks/creatematch';
 import Usermockado from './mocks/user';
 import Teambyidmock from './mocks/team.id';
+import Matchidmock from './mocks/match.id';
 
 import Match from '../database/models/match';
 import User from '../database/models/user';
@@ -86,5 +87,39 @@ describe('3.1 - cadastrando novo jogo com sucesso', () => {
     expect(chaiHttpResponse.body).to.have.property('homeTeamGoals');
     expect(chaiHttpResponse.body).to.have.property('awayTeamGoals');
     expect(chaiHttpResponse.body).to.have.property('inProgress');
+  });
+});
+
+describe('3.2 - atualiza jogo', () => {
+  let chaiHttpResponse: Response;
+
+  before(async () => {
+    sinon
+      .stub(User, 'findOne')
+      .resolves({...Usermockado, password: undefined} as unknown as User);
+    sinon
+      .stub(Match, 'findOne')
+      .resolves(Matchidmock as unknown as Match);
+    sinon
+      .stub(Match, 'update')
+      .resolves();
+  });
+
+  after(() => {
+    (User.findOne as sinon.SinonStub).restore();
+    (Match.findOne as sinon.SinonStub).restore();
+    (Match.update as sinon.SinonStub).restore();
+  });
+
+  it('atualização ok', async () => {
+    chaiHttpResponse = await chai
+      .request(app)
+      .patch('/matches/1/finish')
+      .set(
+        'authorization',
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxLCJ1c2VybmFtZSI6IkFkbWluIiwicm9sZSI6ImFkbWluIiwiZW1haWwiOiJhZG1pbkBhZG1pbi5jb20ifSwiaWF0IjoxNjU0NDQ1NDYzLCJleHAiOjE2NTYxNzM0NjN9.VXyZOjWDTumpESVa8azglcGUXmYPfxCuz6cf2TSp3Ks',
+      )
+
+    expect(chaiHttpResponse.status).to.be.equal(200);
   });
 });

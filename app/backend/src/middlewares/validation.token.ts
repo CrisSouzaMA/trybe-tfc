ece7e3185830b03213078474e0934e68db46365a
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2022-06-07 17:39:34 -0300

    Fix: retira console.log da validação

--- app/backend/src/middlewares/validation.token.ts
+++ app/backend/src/middlewares/validation.token.ts
@@ -16,7 +16,6 @@ export default class ValidationToken {
   private logintoken = new Login();
 
   public validationtoken = async (req: Request, res: Response, next: NextFunction) => {
-    console.log('que coisa mais chata');
     const checktoken = req.headers.authorization;
 
     if (!checktoken) {
@@ -38,7 +37,6 @@ export default class ValidationToken {
     }
 
     req.body.users = checkUser;
-    console.log('quero acabar logo');
     next();
   };
 }

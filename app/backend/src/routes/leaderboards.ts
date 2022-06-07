b64745c3a7935b05f41a802de94990ae6ba23ab3
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2022-06-07 00:49:48 -0300

    Feat: implementa rota de away

--- app/backend/src/routes/leaderboards.ts
+++ app/backend/src/routes/leaderboards.ts
@@ -1,13 +1,20 @@
 import { Application } from 'express';
 import Homeleadercontroller from '../controllers/leaderboards.controller';
+import Awayleadercontroller from '../controllers/leaderaway.controller';
 
 export default class Leaderrouter {
   private controller = new Homeleadercontroller();
+  private awaycontrollers = new Awayleadercontroller();
 
   public route = (app: Application) => {
     app.get(
       '/leaderboard/home',
       (req, res) => this.controller.homecontroller(req, res),
+    );
+
+    app.get(
+      '/leaderboard/away',
+      (req, res) => this.awaycontrollers.awaycontroller(req, res),
     );
   };
 }

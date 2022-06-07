9c71be9afddb27122c07e064eeef259779bcf412
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2022-06-07 00:08:14 -0300

    Feat: implementa conroller de leaderboard

---
+++ app/backend/src/controllers/leaderboards.controller.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import Homeleader from '../services/leader.service';
+
+export default class Homeleadercontroller {
+  private service = new Homeleader();
+
+  public homecontroller = async (_req: Request, res: Response) => {
+    const hcontroll = await this.service.classification();
+    return res.status(StatusCodes.OK).json(hcontroll);
+  };
+}
